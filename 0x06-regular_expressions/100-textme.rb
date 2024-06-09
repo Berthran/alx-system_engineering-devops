fd906e8014373b7db1fd56341fd7c1063a3d729c
Daniel Berthran <berthlearn@gmail.com>
2024-06-09 16:10:14 +0100

    remove spaces between extracted groups

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,4 +2,4 @@
 SENDER = ARGV[0].scan(/\[from:(.*?)\]/).join
 RECEIVER = ARGV[0].scan(/\[to:(.*?)\]/).join
 FLAGS = ARGV[0].scan(/\[flags:(.*?)\]/).join
-puts "#{SENDER}, #{RECEIVER}, #{FLAGS}"
+puts "#{SENDER},#{RECEIVER},#{FLAGS}"
