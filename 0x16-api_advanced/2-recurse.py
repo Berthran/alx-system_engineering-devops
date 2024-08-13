7c739d70efa05aa4716526a97e0bc96eb82987c5
Daniel Berthran <berthlearn@gmail.com>
2024-08-13 13:43:56 +0100

    add imports

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,4 +5,7 @@ Stores the titles of all hot articles for a given subreddit
 # Check 1: File exist
 # Check 2: Shebang
 # Check 3: Pycodestyle
-# Check 4: 
+# Check 4: Module doc
+
+import requests
+import sys
