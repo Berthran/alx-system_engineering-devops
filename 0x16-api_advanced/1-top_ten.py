baf9409da73f96b5924114472614191c0755a52f
Daniel Berthran <berthlearn@gmail.com>
2024-08-13 14:28:35 +0100

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,15 +3,8 @@
 Gets the title of the first 10 hot posts listed for a given subreddit
 '''
 
-
 import requests
 import sys
-
-
-def main():
-    '''Calls the top_ten function'''
-    subreddit = sys.argv[1]
-    top_ten(subreddit)
 
 
 def top_ten(subreddit):
@@ -19,15 +12,35 @@ def top_ten(subreddit):
     Queries the reddit API and prints the titles of the first 10 hot posts
     listed for a given subreddit
     '''
+    if not subreddit:
+        return
 
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=9'.format(subreddit)
-    response = requests.get(url, allow_redirects=False)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
+    params = {'limit': 10}
+
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+
+    response = requests.get(url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
+
     if response.status_code != 200:
         print("None")
     else:
-        children = response.json().get('data').get('children')
+        main_data = response.json()
+        data = main_data.get('data')
+        children = data.get('children')
+
         for child in children:
             print(child.get('data').get('title'))
+
+
+def main():
+    '''Calls the top_ten function'''
+    subreddit = sys.argv[1]
+    top_ten(subreddit)
 
 
 if __name__ == "__main__":
