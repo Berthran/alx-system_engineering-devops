148aa9efcdac2d4eb9467d04a7065aaa355bb3eb
Daniel Berthran <berthlearn@gmail.com>
2024-08-13 14:22:01 +0100

    recursive call to reddit API

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,3 +9,50 @@ Stores the titles of all hot articles for a given subreddit
 
 import requests
 import sys
+
+
+def recurse(subreddit, after=None, hot_list=[]):
+    '''
+    Returns a list containing the titles of all hot articles
+    '''
+    if not subreddit:
+        return
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    params = {}
+
+    if after:
+        params['after'] = after
+
+    response = requests.get(url,
+                            headers=headers,
+                            params=params, allow_redirects=False)
+
+    if response.status_code != 200:
+        return None
+    main_data = response.json()
+
+    data = main_data.get("data")
+
+    children = data.get("children")
+
+    for child in children:
+        hot_list.append(child.get("data", {}).get("title"))
+
+    after = data.get("after")
+
+    if after:
+        return recurse(subreddit, after, hot_list)
+    return hot_list
+
+
+def main():
+    subreddit = sys.argv[1]
+    recurse(subreddit)
+
+
+if __name__ == "__main__":
+    main()
