30a03d60a91e195b845a8c7854bb6607d69bc07a
Daniel Berthran <berthlearn@gmail.com>
2024-08-15 01:58:30 +0100

    Use try and except blocks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,19 +15,23 @@ def number_of_subscribers(subreddit):
     queries the Reddit API and returns the number of subscribers
     (not active users, total subscribers) for a given subreddit.
     '''
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, allow_redirects=False)
-    if response.status_code == 404:
+    try:
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+        response = requests.get(url, allow_redirects=False)
+        if response.status_code == 404:
+            return (0)
+        subscribers = response.json().get('data', {}).get('subscribers', 0)
+        return (subscribers)
+    except Exception:
+        print('Not Found')
         return (0)
-    subscribers = response.json().get('data', {}).get('subscribers', 0)
-    return (subscribers)
 
 
-def main():
-    '''Calls the number_of_subscribers() function'''
-    subreddit = sys.argv[1]
-    number_of_subscribers(subreddit)
+# def main():
+#     '''Calls the number_of_subscribers() function'''
+#     subreddit = sys.argv[1]
+#     number_of_subscribers(subreddit)
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
