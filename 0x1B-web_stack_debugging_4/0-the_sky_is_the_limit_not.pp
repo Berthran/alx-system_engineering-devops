8db54052a831c4b85f4b3c597897653090d9b5a9
Daniel Berthran <berthlearn@gmail.com>
2024-08-23 02:38:17 +0100

    initial commit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,32 @@
+# Error: Too many open files
+# Check I: current limits
+# Command: ulimit -n
+
+# Define an exec resource to update Nginx configuration for handling more connections
+exec { 'fix--for-nginx':
+  
+  # The command to run: use 'sed' to search and replace '15' with '4096'
+  # in the /etc/default/nginx file. This file likely contains configuration settings
+  # related to file descriptor limits. The change increases the limit from 15 to 4096.
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  
+  # Specify the directories to search for the 'sed' command.
+  # This ensures that Puppet can find 'sed' to execute the command.
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Define an exec resource named 'nginx-restart' to restart the Nginx service
+exec { 'nginx-restart':
+  
+  # Command to execute:
+  # Restart the Nginx service to apply the new configuration changes
+  # The command 'nginx restart' is used here, but in some systems, 
+  # 'nginx -s reload' or a service management command like 'systemctl restart nginx' 
+  # might be more appropriate depending on the system's init system.
+  command => 'nginx restart',
+  
+  # Specify the directory to search for the 'nginx' command.
+  # This path is typically where init scripts are located, but you may need to
+  # adjust this path based on your system's init system or service management tool.
+  path    => '/etc/init.d/'
+}
